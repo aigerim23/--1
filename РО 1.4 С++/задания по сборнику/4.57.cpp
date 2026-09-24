66992c439c2b6529831ad4442e4225aa30e26bfb
lizergide <eldar77013@gmail.com>
2026-09-24 16:54:23 +0500

    задания по сборнику за 24.09

---
+++ РО 1.4 С++/задания по сборнику/4.57.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a, b, c, d, n;
+	cin >> a;
+	if (a < 100 || a >= 999) {
+		cout << "Некорректное значение";
+		return 1;
+	}
+	else {
+		b = a / 100;
+		c = (a / 10) % 10;
+		d = a % 10;
+	}
+	cin >> n;
+	if (b == 6 || c == 6 || d == 6) {
+		cout << "Есть 6";
+	}
+	else if (b == n || c == n || d == n) {
+		cout << "Есть " << n;
+	}
+	else {
+		cout << "Цифр нет";
+	}
+	return 0;
+}
